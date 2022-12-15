a0a0bb386a001eb476748103f22844ffb63cec19
webdevtonym <75320313+webdevtonym@users.noreply.github.com>
2022-12-15 14:14:20 +0000

    added more info to comments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-// create a function that prints number 1-10
+//A for loop has 3 parts:
+
+// a starting point: var i = 0
+// a stop/end point i ≤ 100
+// a “step” i++ (a short version of i = i+1)
 
 // for(declare variable; input operation; increment by 1) {
     // log variable to the console
 // }
+
+//Then invoke the created function
+
+
+// create a function that prints number 1-10
+
 
 function printNumbers() {
     for(var i=1; i <= 10; i++) {
@@ -10,3 +20,4 @@ function printNumbers() {
     }
 }
 
+printNumbers(); //invoke function
