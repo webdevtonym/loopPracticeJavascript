29e3026e0ce41a5c731815e603053e5ba6e4b952
webdevtonym <75320313+webdevtonym@users.noreply.github.com>
2022-12-15 14:05:54 +0000

    added comments and function to print numbers from 1-10

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// create a function that prints number 1-10
+
+// for(declare variable; input operation; increment by 1) {
+    // log variable to the console
+// }
+
+function printNumbers() {
+    for(var i=1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
